d889b93072590e02c9121f47382270900c89e808
matianci <1255986454@qq.com>
2020-12-09 12:05:45 +0800

    1209上午

--- dist/js/gouwuche.js
+++ dist/js/gouwuche.js
@@ -21,7 +21,9 @@ $(function () {
                         <div class="jian" data-id="${v.pid}">
                             -
                         </div>
-                        <input type="text" value="${v.pnum}" class="zhi" data-num="${v.pnum}">
+                        <input type="text" value="${
+                          v.pnum
+                        }" class="zhi" data-num="${v.pnum}">
                         <div class="jia" data-id="${v.pid}">
                             +
                         </div>
@@ -33,112 +35,128 @@ $(function () {
                 </li>`;
         });
         $("#car_box").html(html);
-        schanshu();//删除商品
-        clickchoose();//复选框判断
-        dataAdd();//增加商品
-        dataReduce();//减少商品
+        schanshu(); //删除商品
+        clickchoose(); //复选框判断
+        dataAdd(); //增加商品
+        dataReduce(); //减少商品
+        jiean();//结算按钮
       },
     });
   }
   mm();
   // 单价和总价的计算
- 
-
-
 
   //减少商品
-function dataReduce() {
-    $(".jian").click(function() {
-        let nowNum = $(this).next().attr('data-num')
-        if ($(this).next().val() - 0 == 1) {
-            return
-        }
-        $(this).next().attr('data-num', nowNum - 1)
-        $.ajax({
-            url: "http://jx.xuzhixiang.top/ap/api/cart-update-num.php",
-            type: "Get",
-            data: {
-                uid: localStorage.getItem('u-id'),
-                pid: $(this).attr('data-id'),
-                pnum: nowNum - 1
-            },
-            success: () => {
-                $(this).next().val($(this).next().val() - 1)
-                $(this).parent().next().html(parseInt(parseInt($(this).parent().prev().html()) * $(this).next().val()))
-                getTotalPrice()
-            },
-            error: (error) => {
-                console.log(error)
-            }
-        })
-    })
-}
+  function dataReduce() {
+    $(".jian").click(function () {
+      let nowNum = $(this).next().attr("data-num");
+      if ($(this).next().val() - 0 == 1) {
+        return;
+      }
+      $(this)
+        .next()
+        .attr("data-num", nowNum - 1);
+      $.ajax({
+        url: "http://jx.xuzhixiang.top/ap/api/cart-update-num.php",
+        type: "Get",
+        data: {
+          uid: localStorage.getItem("u-id"),
+          pid: $(this).attr("data-id"),
+          pnum: nowNum - 1,
+        },
+        success: () => {
+          $(this)
+            .next()
+            .val($(this).next().val() - 1);
+          $(this)
+            .parent()
+            .next()
+            .html(
+              parseInt(
+                parseInt($(this).parent().prev().html()) * $(this).next().val()
+              )
+            );
+          getTotalPrice();
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
+    });
+  }
 
-//增加商品
-function dataAdd() {
-    $(".jia").click(function() {
-        let nowNum = $(this).prev().attr('data-num');
-        $(this).prev().attr('data-num', nowNum - 0 + 1)
-        $.ajax({
-            url: "http://jx.xuzhixiang.top/ap/api/cart-update-num.php",
-            type: "Get",
-            data: {
-                uid: localStorage.getItem('u-id'),
-                pid: $(this).attr('data-id'),
-                pnum: +nowNum + 1
-            },
-            success: () => {
-                $(this).prev().val($(this).prev().val() - 0 + 1);
-                $(this).parent().next().html(parseInt($(this).parent().prev().html()) * $(this).prev().val());
-                getTotalPrice()
-            },
-            error: (error) => {
-                console.log(error)
-            }
-        })
+  //增加商品
+  function dataAdd() {
+    $(".jia").click(function () {
+      let nowNum = $(this).prev().attr("data-num");
+      $(this)
+        .prev()
+        .attr("data-num", nowNum - 0 + 1);
+      $.ajax({
+        url: "http://jx.xuzhixiang.top/ap/api/cart-update-num.php",
+        type: "Get",
+        data: {
+          uid: localStorage.getItem("u-id"),
+          pid: $(this).attr("data-id"),
+          pnum: +nowNum + 1,
+        },
+        success: () => {
+          $(this)
+            .prev()
+            .val($(this).prev().val() - 0 + 1);
+          $(this)
+            .parent()
+            .next()
+            .html(
+              parseInt($(this).parent().prev().html()) * $(this).prev().val()
+            );
+          getTotalPrice();
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
+    });
+  }
 
-    })
-}
-  
-//计算总价
-function getTotalPrice() {
+  //计算总价
+  function getTotalPrice() {
     var sum = 0;
     for (let i = 0; i < $(".checked-A-btn").length; i++) {
-        if ($('.checked-A-btn').eq(i).prop('checked')) { //判断单选按钮是否勾选
-            sum += +$(".xiaoji").eq(i).html(); //求和
-        }
+      if ($(".checked-A-btn").eq(i).prop("checked")) {
+        //判断单选按钮是否勾选
+        sum += +$(".xiaoji").eq(i).html(); //求和
+      }
     }
     $("#zongjia").html(sum);
-}
+  }
 
-// -------------------------------------------------------
-   //选择框判断
+  // -------------------------------------------------------
+  //选择框判断
 
-   function clickchoose() {
+  function clickchoose() {
     var totoalchoose = document.querySelector("#quan");
     var achoose = document.querySelectorAll(".choose"); //单选框
     //点击全选的时候将所有单选框勾选上
     totoalchoose.onclick = function () {
-        
       for (let j = 0; j < achoose.length; j++) {
         if (totoalchoose.checked) {
           achoose[j].checked = true;
         } else {
           achoose[j].checked = false;
         }
-      }getTotalPrice()
+      }
+      getTotalPrice();
     };
 
     //调用的时候遍历一遍单选按钮
     for (let i = 0; i < achoose.length; i++) {
-        
       let flag = true;
       if (!achoose[i].checked) {
         flag = false;
         break;
       }
       totoalchoose.checked = flag;
-      
     }
 
     //判断勾选的单选框的选择情况，有一个没选择全选按钮就不勾选
@@ -156,7 +174,7 @@ function getTotalPrice() {
       };
     }
   }
-  
+
   // 删除商品
   function schanshu() {
     let shan = document.getElementsByClassName("shan");
@@ -173,10 +191,10 @@ function getTotalPrice() {
           success: function () {
             clickchoose();
             getTotalPrice();
-            if($(".jia").length==0){
-                var totoalchoose = document.querySelector("#quan");
+            if ($(".jia").length == 0) {
+              var totoalchoose = document.querySelector("#quan");
 
-                totoalchoose.checked = false;
+              totoalchoose.checked = false;
             }
           },
         });
@@ -185,4 +203,93 @@ function getTotalPrice() {
     }
   }
 
+  // 结算
+  function jiesuan() {
+    console.log("aaaaaaa");
+    for (let i = 0; i < $(".checked-A-btn").length; i++) {
+      if ($(".checked-A-btn").eq(i).prop("checked")) {
+        //判断单选按钮是否勾选
+        // sum += +$(".xiaoji").eq(i).html(); //求和
+        let paid = $(".shan").eq(i).attr("li_id");
+        $.ajax({
+          type: "Get",
+          url: "http://jx.xuzhixiang.top/ap/api/cart-delete.php",
+          data: {
+            uid: localStorage.getItem("u-id"),
+            pid: paid,
+          },
+          success: function () {
+           
+
+            var totoalchoose = document.querySelector("#quan");
+
+            totoalchoose.checked = false;
+            $("#zongjia").text(0);
+          },
+        });
+        mm();
+      }
+    }
+  }
+
+  // 判断本地有没有钱
+  function qian() {
+    if (localStorage.getItem("u-id")) {
+      let uu = localStorage.getItem("u-id");
+      console.log(uu);
+      if (localStorage.getItem(`${uu}`)) {
+        let qian = localStorage.getItem(`${uu}`);
+        $("#yu").text(qian);
+      } else {
+        localStorage.setItem(`${uu}`, 0);
+      }
+    }
+  }
+  qian(); //调用
+
+  function jianqian() {
+    let fuqian = Number($("#zongjia").text());
+    let youqian = Number($("#yu").text());
+    let shengxia = youqian - fuqian;
+    console.log(shengxia);
+    $("#yu").text(shengxia);
+    let uu = localStorage.getItem("u-id");
+    localStorage.setItem(`${uu}`, shengxia);
+  }
+  // 结算按钮函数
+  function jiean(){
+  $(".jiesuan").click(function () {
+    clickchoose(); //复选框
+    getTotalPrice(); //总价
+    console.log($("#yu").text());
+    console.log($("#zongjia").text());
+    console.log(Number($("#yu").text()) < Number($("#zongjia").text()));
+    if (Number($("#yu").text()) > Number($("#zongjia").text())) {
+      jianqian();
+
+      jiesuan();
+    }else if (Number($("#yu").text()) < Number($("#zongjia").text())) {
+      $(".chongzhi").css({
+        display: "block",
+      });
+      $("#quxiao").click(function () {
+        $(".chongzhi").css({
+          display: "none",
+        });
+      });
+
+      // 充值按钮
+      $("#chong").click(function () {
+        let uid = localStorage.getItem("u-id");
+        console.log(uid);
+        let yue = Number(localStorage.getItem(`${uid}`)); //获取本地余额
+        console.log(yue);
+        let xin = $("#chongzhi").val();
+        yue += Number(xin);
+        localStorage.setItem(`${uid}`, yue);
+        qian();
+      });
+    }
+  });
+}
 });
--- dist/js/xiangqing.js
+++ dist/js/xiangqing.js
@@ -19,6 +19,7 @@
     },
     success: function(res) {
         $('.xq_tu img').attr('src', `../${res.data.pimg}`)
+        $('.xq_tu1 img').attr('src', `../${res.data.pimg}`)
         $('#mingcheng').html(`${res.data.pname}`)
         $('#miaoshu').html(`${res.data.pdesc}`)
         $('#jiage span').html(`${res.data.pprice}`)
@@ -63,9 +64,58 @@ $.ajax({
 }else{
     location.href = "denglu.html";
     alert("您还未登录，请先登录再加入购物车");
-    
-
 }
+}
+var box1 = document.querySelector(`.xq_tu`);
+var lens = document.querySelector(`.lens`);
+var pic = document.querySelector(`.xq_tu1 img`);
+window_drage(lens, box1, pic, 2);
 
-
+// 放大镜效果,box(模拟透镜),box1(存放需要放大的图片的盒子),pic(放大区域的图片),mul(放大倍数)
+// 要给模拟透镜,放大区域的图片添加绝对定位
+function window_drage(box, box1, pic, mul) {
+  box1.onmouseenter = function (e0) {
+    let evt0 = e0 || window.e0;
+    // 在可视区域点击时，模拟透镜中心位置出现在鼠标点击处
+    pic.style.opacity = 1;
+    $(".xq_tu1").css({
+        "display":"block"
+    })
+    // box.style.left = evt0.clientX - box.offsetWidth / 2-box1.offsetLeft + `px`;
+    // box.style.top = evt0.clientY - box.offsetHeight / 2 -box1.offsetTop+ `px`;
+    // 放大区域图片可见，并显示放大位置
+    box.style.backgroundColor = `rgba(167, 188, 194,0.8)`;
+    // 移动模拟透镜，放大区域图片按比例进行位移,并且将模拟透镜的唯一区域固定在box1中
+    box1.onmousemove = function (e2) {
+      let evt2 = e2 || window.e2;
+      let l = evt2.clientX - box.offsetWidth / 2 - box1.offsetLeft;
+      let t = evt2.clientY - box.offsetHeight / 2 - box1.offsetTop;
+      if (l >= box1.offsetWidth - box.offsetWidth) {
+        l = box1.offsetWidth - box.offsetWidth;
+      }
+      if (l <= 0) {
+        l = 0;
+      }
+      if (t >= box1.offsetHeight - box.offsetHeight) {
+        t = box1.offsetHeight - box.offsetHeight;
+      }
+      if (t <= 0) {
+        t = 0;
+      }
+      box.style.left = l + "px";
+      box.style.top = t + "px";
+      pic.style.left = -l * mul + "px";
+      pic.style.top = -t * mul + "px";
+    };
+    // 鼠标松开模拟透镜消失，放大区域消失
+    box1.onmouseleave = function () {
+      window.onmousemove = null;
+      box.style.backgroundColor = `rgba(167, 188, 194,0)`;
+      pic.style.opacity = 0;
+      pic.style.opacity = 1;
+      $(".xq_tu1").css({
+          "display":"none"
+      })
+    };
+  };
 }
